780b48024170efcbed8133031d05a276cf679af8
stoled0 <sarahalmeida6501@gmail.com>
2026-08-31 20:18:07 -0300

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void josefo(int n) {
+    if (n == 1) {
+        return 0; //caso base
+    }   
+    else {
+        
+        // passo recursivo
+    }
+}
+
+int main()
+{
+    return 0;
+}
+
+// PILHA DE EXECUÇÃO
+// josefo(ABCDEF) = return 6-1 = return 5
+// josefo(CDEFA) = return 4
+// josefo(EFAC) = return 3
+// josefo(ACE) = return 2
+// josefo(EA) = return 1
+// josefo(E) = return 0
